1eb2a4ea248487beb03b4caa3da1eb29f6be7f26
Spydragon23 <kcmacint@ualberta.ca>
2025-10-29 23:53:33 -0600

    Added FireStore helper class

--- LotteryEventApp/app/src/main/java/com/ijaskz/lotteryeventapp/Event.java
+++ LotteryEventApp/app/src/main/java/com/ijaskz/lotteryeventapp/Event.java
@@ -2,16 +2,16 @@ package com.ijaskz.lotteryeventapp;
 
 public class Event {
     private String event_description;
-    private int event_id;
+    private String event_id;
     private String location;
     private String event_name;
     private int max;
     private String event_time;
     public Event(){}
 
-    public Event(String event_description, int event_id, String location,  String event_name, int max, String event_time){
+    public Event(String event_description, String location,  String event_name, int max, String event_time){
         this.event_description = event_description;
-        this.event_id = event_id;
+        //this.event_id = event_id;
         this.location = location;
         this.event_name = event_name;
         this.max = max;
@@ -22,7 +22,7 @@ public class Event {
         return event_description;
     }
 
-    public int getEvent_id() {
+    public String getEvent_id() {
         return event_id;
     }
 

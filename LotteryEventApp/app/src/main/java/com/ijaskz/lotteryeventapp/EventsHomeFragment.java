5d034a554fa73cf151f56a2e915efeb68cf5ded4
Spydragon23 <kcmacint@ualberta.ca>
2025-10-29 23:57:15 -0600

    Added FireStore helper class

--- LotteryEventApp/app/src/main/java/com/ijaskz/lotteryeventapp/EventsHomeFragment.java
+++ LotteryEventApp/app/src/main/java/com/ijaskz/lotteryeventapp/EventsHomeFragment.java
@@ -2,6 +2,7 @@ package com.ijaskz.lotteryeventapp;
 
 // EventsHomeFragment.java
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,37 +13,33 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventsHomeFragment extends Fragment {
+    private RecyclerView rvEvents;
+    private EventsAdapter adapter;
+    private FireStoreHelper dbHelper = new FireStoreHelper();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.events_home, container, false);
+        View v = inflater.inflate(R.layout.events_home, container, false);
+        rvEvents = v.findViewById(R.id.rvEvents);
+        rvEvents.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new EventsAdapter();
+        rvEvents.setAdapter(adapter);
+        // How adding an event will work
+        //Event event = new Event("Basketball in gym", "gym","Basketball 5v5 training", 25, "5pm");
+        //dbHelper.addEvent(event);
+        //displaying the events
+        dbHelper.getEvents(adapter);
 
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewEvents);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new RecyclerView.Adapter() {
-            @NonNull
-            @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View itemView = inflater.inflate(R.layout.item_event, parent, false);
-                return new RecyclerView.ViewHolder(itemView) {};
-            }
-
-            @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-                // Items will show with default text from XML
-            }
-
-            @Override
-            public int getItemCount() {
-                return 5; // Shows 5 items
-            }
-        });
-
-
-
-
-        return view;
+        return v;
     }
+
+
 }

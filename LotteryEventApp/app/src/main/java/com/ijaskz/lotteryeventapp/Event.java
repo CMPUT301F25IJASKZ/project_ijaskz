6c57a77c5716d813bdf10257fd768d04842f3994
Spydragon23 <kcmacint@ualberta.ca>
2025-10-29 19:15:37 -0600

    Added Event Class

---
+++ LotteryEventApp/app/src/main/java/com/ijaskz/lotteryeventapp/Event.java
@@ -0,0 +1,44 @@
+package com.ijaskz.lotteryeventapp;
+
+public class Event {
+    private String event_description;
+    private int event_id;
+    private String location;
+    private String event_name;
+    private int max;
+    private String event_time;
+    public Event(){}
+
+    public Event(String event_description, int event_id, String location,  String event_name, int max, String event_time){
+        this.event_description = event_description;
+        this.event_id = event_id;
+        this.location = location;
+        this.event_name = event_name;
+        this.max = max;
+        this.event_time = event_time;
+    }
+
+    public String getEvent_description() {
+        return event_description;
+    }
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public String getEvent_time() {
+        return event_time;
+    }
+}
